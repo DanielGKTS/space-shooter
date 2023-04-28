e46aa51e4abd32c856ed7ae792956afc57315c84
SoloDraco <99818483+SoloDraco@users.noreply.github.com>
2023-04-28 00:00:09 -0400

    Update Form1.cs

--- Space Shooter/Space Shooter/Form1.cs
+++ Space Shooter/Space Shooter/Form1.cs
@@ -1,75 +1,75 @@
-using static System.Formats.Asn1.AsnWriter;
+using static System.Formats.Asn1.AsnWriter; // Defining the namespace for the code
 
-namespace Space_Shooter
+namespace Space_Shooter // Defining the namespace for the code
 {
-	public partial class Form1 : Form
+	public partial class Form1 : Form // Defining a class named Form1 that inherits from the Form class
 	{
-		public Form1()
+		public Form1() // Constructor for the Form1 class
 		{
-			InitializeComponent();
-			lbl_over.Hide();
+			InitializeComponent(); // Initializing the Form1 component
+			lbl_over.Hide(); // Hiding the "Game Over" label at the start of the game
 
 		}
 
-		bool right, left, space;
-		int score;
+		bool right, left, space; // Declaring boolean variables to keep track of key presses
+		int score; // Declaring an integer variable to keep track of the player's score
 
-		void Game_Result()
+		void Game_Result() // Defining a method to check the game's result
 		{
-			foreach (Control j in this.Controls)
+			foreach (Control j in this.Controls) // Looping through all the controls on the form
 			{
-				foreach (Control i in this.Controls)
+				foreach (Control i in this.Controls) 
 				{
-					if (j is PictureBox && j.Tag == "bullet")
+					if (j is PictureBox && j.Tag == "bullet") // Checking if the control is a bullet
 					{
-						if (i is PictureBox && i.Tag == "enemy")
+						if (i is PictureBox && i.Tag == "enemy") // Checking if the control is an enemy ship
 						{
 							if (j.Bounds.IntersectsWith(i.Bounds))
 							{
 								int x;
 								Random rnd = new Random();
 								x = rnd.Next(0, 350);
-								i.Location = new Point(x, 0);
+								i.Location = new Point(x, 0); // Moving the enemy ship to a new random position
 
-								i.Top = -100;
-								((PictureBox)j).Image = Properties.Resources.explosion;
-								score++;
-								lbl_score.Text = "Score : " + score;
+								i.Top = -100;  // Moving the enemy ship off-screen
+								((PictureBox)j).Image = Properties.Resources.explosion; // Displaying an explosion image at the bullet's location
+								score++; // Increasing the player's score
+								lbl_score.Text = "Score : " + score; // Updating the score label
 							}
 						}
 					}
 				}
 			}
-			if (player.Bounds.IntersectsWith(ship.Bounds) || player.Bounds.IntersectsWith(alien.Bounds))
+			if (player.Bounds.IntersectsWith(ship.Bounds) || player.Bounds.IntersectsWith(alien.Bounds)) // Checking if the player collides with an enemy ship
 			{
-				timer1.Stop();
-				lbl_over.Show();
-				lbl_over.BringToFront();
+				timer1.Stop(); // Stopping the game timer
+				lbl_over.Show(); // Displaying the "Game Over" label
+				lbl_over.BringToFront(); // Bringing the "Game Over" label to the front of the form
 			}
 		}
 
-		void Star()
+		void Star() // Defining a method to move the stars in the background
 		{
-			foreach (Control j in this.Controls)
+			foreach (Control j in this.Controls) // Looping through all the controls on the form
 			{
-				if (j is PictureBox && j.Tag == "stars")
+				if (j is PictureBox && j.Tag == "stars") // Checking if the control is a star
 				{
-					j.Top += 10;
-					if (j.Top > 400)
+					j.Top += 10; // Moving the star downwards
+					if (j.Top > 400) // If the star goes off-screen
 					{
-						j.Top = 0;
+						j.Top = 0; // Moving the star to the top of the screen
 					}
 				}
 			}
 		}
 
-		void Add_Bullet()
+		void Add_Bullet() // Defining a method to add bullets to the form
 		{
-			PictureBox bullet = new PictureBox();
-			bullet.SizeMode = PictureBoxSizeMode.AutoSize;
-			bullet.Image = Properties.Resources.bullet_img;
-			bullet.BackColor = System.Drawing.Color.Transparent;
-			bullet.Tag = "bullet";
+			PictureBox bullet = new PictureBox(); // Creating a new picture box for the bullet
+			bullet.SizeMode = PictureBoxSizeMode.AutoSize; // Setting the size mode of the picture box to auto-size
+			bullet.Image = Properties.Resources.bullet_img; // Setting the image of the picture box to the bullet image
+			bullet.BackColor = System.Drawing.Color.Transparent; // Making the background of the picture box transparent
+			bullet.Tag = "bullet"; // Assigning a tag to the picture box
 			bullet.Left = player.Left + 15;
 			bullet.Top = player.Top - 30;
 			this.Controls.Add(bullet);
